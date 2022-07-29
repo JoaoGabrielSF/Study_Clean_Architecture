0f3b65016796a8632415b52379b0d54b6b585b4e
Gabs <84649313+Kirby-Allstars@users.noreply.github.com>
2022-07-29 14:39:41 -0300

    ...

--- src/infra/repo/pet_repository.py
+++ src/infra/repo/pet_repository.py
@@ -1,24 +1,92 @@
+from typing import List
+from src.domain.models.pets import Pets
 from src.infra.config import DBConnectionHandler 
-from src.infra.entities import Pets
+from src.infra.entities import Pets as PetsModel
 
 class PetRepository:
     """ Class to manage Pet Repository """
     
     @classmethod
-    def insert_pet(cls, name:str, specie:str, age:int):
+    def insert_pet(cls, name: str, specie: str, age: int, user_id: int) -> List[Pets]:
         """ Insert data in pet entity
-        :param  -name: pet name
-                -specie: pet specie
-                -age: pet age
+        :param  - name: pet name
+                - specie: pet specie
+                - age: pet age
+                - user_id: id of the owner
+        :return - tuple with new pet inserted
         """
          
         with DBConnectionHandler() as db_connection:
             try:
-                new_pet = Pets(name=name, specie=specie, age=age )
+                new_pet = PetsModel(name=name, specie=specie, age=age, user_id=user_id)
                 db_connection.session.add(new_pet)
                 db_connection.session.commit()
+                
+                return Pets(
+                 id=new_pet.id,
+                 name=new_pet.name,
+                 specie=new_pet.specie.value,
+                 age=new_pet.age,
+                 user_id=new_pet.user_id
+                 
+                )
             except:
                 db_connection.session.rollback()
                 raise          
             finally:
                 db_connection.session.close()
+        
+        return None
+    
+    def select_pet_by_id(cls, id: int = None) -> List[Pets]:
+         """
+        Select data in user entity by id and/or name
+        :param - id: Id of the registry
+        :return - List with Pets selected
+        """    
+         try:
+                query_data = None
+      
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(id=id)
+                        .one()
+                )
+                query_data = [data]
+                    
+                return query_data
+         except:
+                        db_connection.session.rollback()
+                        raise  
+         finally: 
+                    db_connection.session.close()
+                
+         return None
+     
+
+    def select_pet_by_name(cls, name: str = None) -> List[Pets]:
+         """
+        Select data in user entity by id and/or name
+        :param  - name: Pet name
+        :return - List with Pets selected
+        """
+         try:
+                query_data = None
+      
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(name=name)
+                        .one()
+                )
+                query_data = [data]
+                    
+                return query_data
+         except:
+                        db_connection.session.rollback()
+                        raise  
+         finally: 
+                    db_connection.session.close()
+                
+         return None
---
+++ src/infra/repo/pet_repository_test.py
@@ -0,0 +1,38 @@
+import enum
+from faker import Faker
+from src.infra.config import DBConnectionHandler
+from src.infra.entities.pets import AnimalType
+from .pet_repository import PetRepository
+from src.infra.entities import Pets as PetsModel
+
+
+faker = Faker()
+pet_repository = PetRepository()
+db_connection_handler = DBConnectionHandler()
+
+def test_insert_pet():
+    """Should Insert Pet"""
+
+    name = faker.name()
+    specie = 'fish'
+    age = faker.random_number(digits=2)
+    user_id = faker.random_number()
+    engine = db_connection_handler.get_engine()
+    
+   
+    #SQL Commands 
+    new_pet = pet_repository.insert_pet(name, specie, age, user_id)
+    engine = db_connection_handler.get_engine()
+    query_pet = engine.execute("SELECT * FROM pets WHERE id='{}';".format(new_pet.id)).fetchone()
+    
+    print(new_pet)
+    print(query_pet)
+    
+    assert new_pet.id == query_pet.id
+    assert new_pet.name == query_pet.name
+    assert new_pet.specie == query_pet.specie
+    assert new_pet.age == query_pet.age
+    assert new_pet.user_id == query_pet.user_id
+    
+    engine.execute("DELETE FROM users WHERE id= '{}';".format(new_pet.id))
+    
